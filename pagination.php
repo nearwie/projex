8e0ccede5670e15a14e90f3c12b2d759e4a052f0
nearwie <51052232+nearwie@users.noreply.github.com>
2019-06-30 22:55:40 +0700

    Add files via upload

---
+++ pagination.php
@@ -0,0 +1,28 @@
+$config['base_url'] = base_url('controller/method/');        
+$config['total_rows'] = $this->your_model->method_in_your_model();      
+$config['per_page'] = 2;        
+$config['uri_segment'] = 3;        
+$config['full_tag_open'] = '<ul class="pagination">';        
+$config['full_tag_close'] = '</ul>';  
+$config['attributes'] = array('class' => 'page-link');   
+$config['first_link'] = 'First';        
+$config['last_link'] = 'Last';   
+$config['first_tag_open'] = '<li>';        
+$config['first_tag_close'] = '</li>';        
+$config['prev_link'] = '&laquo';        
+$config['prev_tag_open'] = '<li class="prev">';        
+$config['prev_tag_close'] = '</li>';        
+$config['next_link'] = '&raquo';        
+$config['next_tag_open'] = '<li>';        
+$config['next_tag_close'] = '</li>';        
+$config['last_tag_open'] = '<li>';        
+$config['last_tag_close'] = '</li>';        
+$config['cur_tag_open'] = '<li class="page-item active"><a href="#" class="page-link">';        
+$config['cur_tag_close'] = '<span class="sr-only">(current)</span></a></li>';        
+$config['num_tag_open'] = '<li>';
+$config['num_tag_close'] = '</li>';
+
+$page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+$this->pagination->initialize($config);        
+$data['links'] = $this->pagination->create_links();     
+$data['employees'] = $this->your_model->method_in_your_model($config["per_page"], $page);  
